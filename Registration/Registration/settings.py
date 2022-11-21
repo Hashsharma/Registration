eb20bd4959ecfd98795c21bb121e891b12537fff
scientist <hashsharma0@gmail.com>
2022-11-21 23:43:56 +0530

    Django Heroku configuration error

--- Registration/Registration/settings.py
+++ Registration/Registration/settings.py
@@ -194,7 +194,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-django_heroku.settings(locals(), staticfiles=False)
+django_heroku.settings(locals())
 
 
 # Default primary key field type
